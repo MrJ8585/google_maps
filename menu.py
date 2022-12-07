416b091c393df6e9c99fbdb1e22edbe7a04d885c
MrJ8585 <josea.solis@cetys.edu.mx>
2022-12-07 01:43:00 -0800

    watafac me quieroo morir
    ayudame Dios mio noo se que estoy haciendo con mi vida

--- menu.py
+++ menu.py
@@ -1,33 +1,59 @@
 import tkinter
 from tkinter import *
+#from tkintermapview import TkinterMapView
+from mapachido import mapa
+
+def get_inputs():
+    a = number_locations.get(1.0, END)
+    b = coordenada1.get(1.0, END)
+    c = coordenada2.get(1.0, END)
+    d = coordenada3.get(1.0, END)
+    e = coordenada4.get(1.0, END)
+    return [a,b,c,d,e]
+
+def make_array():
+    out = []
+    m = get_inputs()
+    for i in m:
+        j = i.split()
+        for n in j:
+            out.append(float(n))
+    return out
 
 menu_window = tkinter.Tk()
-menu_window.geometry(f"{300}x{150}")
+menu_window.geometry(f"{400}x{500}")
 menu_window.title("Menu")
 menu_window.resizable(False, False)
 
-def save_text():
-   text_file = open("archivo.txt", "w")
-   text_file.write(input.get(1.0, END))
-   text_file.close()
-
 label = Label(menu_window,text="N. de Ubicacion")
 label.pack()
-input = tkinter.Text(menu_window, height=1, width=15)
-input.pack()
-save = Button(text="Aceptar", command=save_text)
-save.pack()
+number_locations = tkinter.Text(menu_window, height=1, width=25, exportselection=0)
+number_locations.pack()
 
-def save_text2():
-   text_file = open("archivo2.txt", "w")
-   text_file.write(input.get(1.0, END))
-   text_file.close()
-
-label = Label(menu_window,text="Distancia T")
+label = Label(menu_window,text="Coordenada 1")
 label.pack()
-input2 = tkinter.Text(menu_window, height=1, width=15)
-input2.pack()
-save2 = Button(text="Aceptar", command=save_text2)
-save2.pack()
+coordenada1 = tkinter.Text(menu_window, height=1, width=25, exportselection=0)
+coordenada1.pack()
+
+label = Label(menu_window,text="Coordenada 2")
+label.pack()
+coordenada2 = tkinter.Text(menu_window, height=1, width=25, exportselection=0)
+coordenada2.pack()
+
+label = Label(menu_window,text="Coordenada 3")
+label.pack()
+coordenada3 = tkinter.Text(menu_window, height=1, width=25, exportselection=0)
+coordenada3.pack()
+
+label = Label(menu_window,text="Coordenada 4")
+label.pack()
+coordenada4 = tkinter.Text(menu_window, height=1, width=25, exportselection=0)
+coordenada4.pack()
+
+label = Label(menu_window,text="Subir")
+label.pack()
+sumbmit = tkinter.Button(menu_window, height=1, width=25, command=si)
+sumbmit.pack()
+
 
 menu_window.mainloop()
