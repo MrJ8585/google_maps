0367d701ff203716525dc6663c49a4033b2e7e38
el-shago <sperezmaris@gmail.com>
2022-12-07 01:24:19 -0800

    Almacenamiento de datos

--- menu.py
+++ menu.py
@@ -2,18 +2,32 @@ import tkinter
 from tkinter import *
 
 menu_window = tkinter.Tk()
-menu_window.geometry(f"{300}x{100}")
+menu_window.geometry(f"{300}x{150}")
 menu_window.title("Menu")
 menu_window.resizable(False, False)
+
+def save_text():
+   text_file = open("archivo.txt", "w")
+   text_file.write(input.get(1.0, END))
+   text_file.close()
 
 label = Label(menu_window,text="N. de Ubicacion")
 label.pack()
 input = tkinter.Text(menu_window, height=1, width=15)
 input.pack()
+save = Button(text="Aceptar", command=save_text)
+save.pack()
+
+def save_text2():
+   text_file = open("archivo2.txt", "w")
+   text_file.write(input.get(1.0, END))
+   text_file.close()
 
 label = Label(menu_window,text="Distancia T")
 label.pack()
 input2 = tkinter.Text(menu_window, height=1, width=15)
 input2.pack()
+save2 = Button(text="Aceptar", command=save_text2)
+save2.pack()
 
 menu_window.mainloop()
